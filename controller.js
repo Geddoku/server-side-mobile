75c57fd7cb093194d195c085af507126bb59e86a
Nikita <nikitavoznyuk37@gmail.com>
2020-03-13 21:49:24 +0300

    BACK_REWRITING

--- controller.js
+++ controller.js
@@ -5,9 +5,32 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const pubDir = `${__dirname}/public`;
 const io = require('socket.io');
+const path = require('path');
+const WebSocket = require('ws');
 
 const userRoutes = require('./server/routes/users');
 const courseRoutes = require('./server/routes/courses');
+
+const WS_PORT = process.env.WS_PORT || 5001;
+
+const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
+      console.log(`WS server is listening at ws://localhost:${WS_PORT}`));
+
+let connectionClients = [];
+
+wsServer.on('connection', (ws, req) => {
+  console.log('Connected');
+  connectionClients.push(ws);
+  ws.on('message', data => {
+    connectionClients.forEach((ws, i) => {
+      if (ws.readyState === ws.OPEN) {
+        ws.send(data);
+      } else {
+        connectedClients.splice(i, 1);
+      }
+    });
+  });
+});
 
 mongoose.connect('mongodb+srv://dbMOBILE:serverjs@mobile-server-rjy4g.mongodb.net/test?retryWrites=true&w=majority',
 {useNewUrlParser: true} );
@@ -29,14 +52,13 @@ app.use((res, req, next) => {
   next();
 });
 
-app
-  .get('/client', (req, res) => {
-    res.sendFile(`${pubDir}/client.html`)
-  })
-  .get('/streaming', (req, res) => {
-    res.sendFile(`${pubDir}/server.html`)
-  })
+app.get('/client', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client.html'));
+});
 
+app.get('/streaming', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './streaming.html'));
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
